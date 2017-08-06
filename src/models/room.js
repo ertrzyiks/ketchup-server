db415509b9eb57d6db21f85c4820677fc9cd0b7e
ertrzyiks <mateusz.derks@gmail.com>
2017-08-06 20:01:50 +0200

    Clean up user and room model definition

--- src/models/room.js
+++ src/models/room.js
@@ -1,3 +1,12 @@
-import {Room} from './users_rooms'
+import {bookshelf} from '../db'
+import {User} from './user'
+
+const Room = bookshelf.Model.extend({
+  tableName: 'rooms',
+  users: function() {
+    return this.hasMany(User)
+  },
+  hasTimestamps: true
+})
 
 export default Room
--- src/models/user.js
+++ src/models/user.js
@@ -1,3 +1,15 @@
-import {User} from './users_rooms'
+import {bookshelf} from '../db'
+import {Room} from './room'
+
+const User = bookshelf.Model.extend({
+  tableName: 'users',
+  tokens: function() {
+    return this.hasMany(Token)
+  },
+  rooms: function() {
+    return this.hasMany(Room)
+  },
+  hasTimestamps: true
+})
 
 export default User
