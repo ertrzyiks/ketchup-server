a370e959123fc21bbfa3bc5b82f8c2c26d83e704
ertrzyiks <mateusz.derks@gmail.com>
2017-08-27 09:01:58 +0200

    Add healthcheck endpoint

--- src/http_server/index.js
+++ src/http_server/index.js
@@ -4,5 +4,12 @@ import v1 from './v1'
 export default (ketchapp) => {
   const httpServer = express()
   httpServer.use('/v1', v1(ketchapp))
+
+  httpServer.get('/status', (req, res) => {
+    res.send({
+      status: 'ok'
+    })
+  })
+
   return httpServer
 }
