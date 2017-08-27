b2562cfd88d485d268fb90f58d6ab7a7dc9cde2c
ertrzyiks <mateusz.derks@gmail.com>
2017-08-27 18:37:26 +0200

    Add CORS

--- src/http_server/index.js
+++ src/http_server/index.js
@@ -1,8 +1,18 @@
 import express from 'express'
+import cors from 'cors'
 import v1 from './v1'
 
 export default (ketchapp) => {
   const httpServer = express()
+
+  httpServer.use(cors({
+    origin: [
+      'http://localhost:3000',
+      process.env.WEB_URL
+    ],
+    optionsSuccessStatus: 200
+  }))
+
   httpServer.use('/v1', v1(ketchapp))
 
   httpServer.get('/status', (req, res) => {
