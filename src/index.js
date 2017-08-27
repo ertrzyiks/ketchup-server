05e1064c6a24f41bf787c0d32096515d4c57883c
ertrzyiks <mateusz.derks@gmail.com>
2017-08-27 19:20:28 +0200

    Use correct http server for logux server

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import getHttpServer from './http_server'
 const ketchapp = getKetchApp({actions, models})
 const httpServer = getHttpServer(ketchapp)
 
-const app = new KetchupServer(ketchapp, KetchupServer.loadOptions(process, {
-  server: httpServer,
-  subprotocol: '1.0.0',
-  supports: '1.x',
-  root: __dirname
-}))
-
 const listener = httpServer.listen(process.env.PORT || 3000, function () {
+  const app = new KetchupServer(ketchapp, KetchupServer.loadOptions(process, {
+    server: listener,
+    subprotocol: '1.0.0',
+    supports: '1.x',
+    root: __dirname
+  }))
+
   app.reporter.logger.info(listener.address(), 'Custom HTTP Server is listening')
   app.listen()
 })
