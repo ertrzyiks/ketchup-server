ed73c9080c28722694701d2b88ca8a540fc58429
ertrzyiks <mateusz.derks@gmail.com>
2017-08-29 21:33:23 +0200

    Update logux-server and add subscription

--- src/server.js
+++ src/server.js
@@ -21,6 +21,25 @@ export default class KetchupServer extends Server {
   }
 
   _setupTypes() {
+    this.subscription('users/:id', (params, action, meta, creator) => {
+      if (params.id !== creator.user) {
+        return false
+      }
+
+      this.log.add(
+        { type: 'LIST_ROOMS', rooms: [{name: 'Room #1' + creator.user}, {name: 'Room #2' + creator.user}] },
+        { nodeIds: [creator.nodeId], reasons: ['list'] }
+      )
+      return true
+    })
+
+    this.type('LIST_ROOMS', {
+      access: (action, meta) => {
+        return Promise.resolve(true)
+      },
+      process: (action, meta, creator) => {}
+    })
+
     this.type('CREATE_ROOM', {
       access: (action, meta) => {
         return Promise.resolve(true)
