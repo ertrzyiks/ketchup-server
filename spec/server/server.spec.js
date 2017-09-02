38c41dd1f7b03425a121fac734d124cc9e9338da
ertrzyiks <mateusz.derks@gmail.com>
2017-09-02 11:47:57 +0200

    Uncomment spec

--- spec/server/server.spec.js
+++ spec/server/server.spec.js
@@ -27,16 +27,16 @@ test(async t => {
   t.deepEqual(reporter.names, ['connect', 'unauthenticated', 'disconnect'])
 })
 
-// test(async t => {
-//   const reporter = LoguxTestUtils.createReporter(serverFactory)
-//   server = reporter.app
-//
-//   const client = LoguxTestUtils.createClient(reporter.app)
-//   await client.connection.connect()
-//
-//   const user = await createUser(app, {name: '10', accessToken: 'token'})
-//
-//   await LoguxTestUtils.connect(client, user.hash + ':uuid', 'token')
-//
-//   t.deepEqual(reporter.names, ['connect', 'authenticated'])
-// })
+test(async t => {
+  const reporter = LoguxTestUtils.createReporter(serverFactory)
+  server = reporter.app
+
+  const client = LoguxTestUtils.createClient(reporter.app)
+  await client.connection.connect()
+
+  const user = await createUser(app, {name: '10', accessToken: 'token'})
+
+  await LoguxTestUtils.connect(client, user.hash + ':uuid', 'token')
+
+  t.deepEqual(reporter.names, ['connect', 'authenticated'])
+})
